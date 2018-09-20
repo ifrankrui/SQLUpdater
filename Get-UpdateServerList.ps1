2ad98172353a205cbf6eb1c793856284fa749b6f
ifrankrui <42246789+ifrankrui@users.noreply.github.com>
2018-09-20 15:43:21 +0100

    Add files via upload

---
+++ Get-UpdateServerList.ps1
@@ -0,0 +1,56 @@
+﻿function Get-UpdateServerList
+{
+
+[CmdletBinding()]
+
+    param 
+    (
+    [Parameter(Mandatory)]
+    [String] $CSVName  ##CSV name includes CSV file path and anme
+    )
+    process
+    {
+    try
+        {
+        $List = Import-Csv $CSVName
+
+        ForEach ($item in $List)
+
+            {
+            
+            $ComputerName = $item.("ComputerName")
+            $InstanceName = $item.("InstanceName")
+            $Version=$item.("Version")
+            $ServicePack=$item.("ServicePack")
+            $CU=$item.("CU")
+
+            Update-SqlServer -ComputerName $ComputerName -InstanceName $InstanceName -ServicePack $ServicePack -CumulativeUpdate $CU
+            }
+
+        } 
+    catch
+        {
+        ##echo "$(Get-date)      $_"
+        Erroroutput -Exception $_  -ErrorDetails $_
+        }
+    }
+}
+
+
+function ErrorOutput
+{
+Param 
+(
+ [Parameter(Mandatory)]
+ [String] $Exception,
+ [System.Management.Automation.ErrorRecord] $ErrorDetails
+)
+
+
+"$(Get-date)      $Exception" | Out-File $PSScriptRoot\Output.txt -Append
+$ErrorDetails | Out-File $PSScriptRoot\Output.txt -Append
+
+}
+
+Get-UpdateServerList
+
